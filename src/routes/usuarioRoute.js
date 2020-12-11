f8eea2205a018ec867f30ee8e653299d0c28fc0a
nanda-oliveira <fernandaoliveiraufpe@gmail.com>
2020-12-11 11:22:47 -0300

    edit usuario router

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/usuarioController");
 
 router.get("/usuarios", controller.getAllUsers)//http://localhost:3030/usuario/usuarios/
-router.get("/nome", controller.getByName) // http://localhost:3030/usuario/nome
+router.get("/nome/:nome", controller.getByName) // http://localhost:3030/usuario/nome
 router.post("/create", controller.addUser) //http://localhost:3030/usuario/cadastro/
 router.delete("/delete", controller.deleteUser)//http://localhost:3030/usuario/delete/
 router.patch("/update", controller.updateUser) //http://localhost:3030/usuario/atualizar7
